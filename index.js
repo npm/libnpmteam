68342f842c0961bb60dc8c6f8731238c5a69a8ea
Michael Perrotte <mike@npmjs.com>
2020-03-02 16:10:45 -0500

    fix: remove unnecessary promises [PR feedback]

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const eu = encodeURIComponent
 const npmFetch = require('npm-registry-fetch')
 const validate = require('aproba')
 
-const cmd = module.exports = {}
+const cmd = module.exports
 
 cmd.create = (entity, opts = {}) => {
   return Promise.resolve().then(() => {
@@ -21,51 +21,42 @@ cmd.create = (entity, opts = {}) => {
 }
 
 cmd.destroy = (entity, opts = {}) => {
-  return Promise.resolve().then(() => {
-    const { scope, team } = splitEntity(entity)
-    validate('SSO', [scope, team, opts])
-    const uri = `/-/team/${eu(scope)}/${eu(team)}`
-    return npmFetch.json(uri, {
-      ...opts,
-      method: 'DELETE',
-      scope
-    })
+  const { scope, team } = splitEntity(entity)
+  validate('SSO', [scope, team, opts])
+  const uri = `/-/team/${eu(scope)}/${eu(team)}`
+  return npmFetch.json(uri, {
+    ...opts,
+    method: 'DELETE',
+    scope
   })
 }
 
 cmd.add = (user, entity, opts = {}) => {
-  return Promise.resolve().then(() => {
-    const { scope, team } = splitEntity(entity)
-    validate('SSO', [scope, team, opts])
-    const uri = `/-/team/${eu(scope)}/${eu(team)}/user`
-    return npmFetch.json(uri, {
-      ...opts,
-      method: 'PUT',
-      scope,
-      body: { user }
-    })
+  const { scope, team } = splitEntity(entity)
+  validate('SSO', [scope, team, opts])
+  const uri = `/-/team/${eu(scope)}/${eu(team)}/user`
+  return npmFetch.json(uri, {
+    ...opts,
+    method: 'PUT',
+    scope,
+    body: { user }
   })
 }
 
 cmd.rm = (user, entity, opts = {}) => {
-  return Promise.resolve().then(() => {
-    const { scope, team } = splitEntity(entity)
-    validate('SSO', [scope, team, opts])
-    const uri = `/-/team/${eu(scope)}/${eu(team)}/user`
-    return npmFetch.json(uri, {
-      ...opts,
-      method: 'DELETE',
-      scope,
-      body: { user }
-    })
+  const { scope, team } = splitEntity(entity)
+  validate('SSO', [scope, team, opts])
+  const uri = `/-/team/${eu(scope)}/${eu(team)}/user`
+  return npmFetch.json(uri, {
+    ...opts,
+    method: 'DELETE',
+    scope,
+    body: { user }
   })
 }
 
-cmd.lsTeams = (scope, opts = {}) => {
-  return Promise.resolve().then(() => {
-    return cmd.lsTeams.stream(scope, { ...opts }).collect()
-  })
-}
+cmd.lsTeams = (...args) => cmd.lsTeams.stream(...args).collect()
+
 cmd.lsTeams.stream = (scope, opts = {}) => {
   validate('SO', [scope, opts])
   const uri = `/-/org/${eu(scope)}/team`
@@ -75,11 +66,8 @@ cmd.lsTeams.stream = (scope, opts = {}) => {
   })
 }
 
-cmd.lsUsers = (entity, opts = {}) => {
-  return Promise.resolve().then(() => {
-    return cmd.lsUsers.stream(entity, { ...opts }).collect()
-  })
-}
+cmd.lsUsers = (...args) => cmd.lsUsers.stream(...args).collect()
+
 cmd.lsUsers.stream = (entity, opts = {}) => {
   const { scope, team } = splitEntity(entity)
   validate('SSO', [scope, team, opts])
